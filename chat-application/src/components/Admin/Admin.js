504e44885989164df0994e0cc16d7345d1a025c7
Sir Angel Naguit <angelnaguit@icloud.com>
2020-04-02 16:11:53 -0400

    css attempt

--- chat-application/src/components/Admin/Admin.js
+++ chat-application/src/components/Admin/Admin.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import AdminTabs from './SubComponentsAdmin/tabs';
 
+import "./Admin.css";
+
 const Admin = () => {
     return ( 
-        <>
+        
+        <div id="body">
         <h1>Admin Page</h1>
         <AdminTabs/>
-        </>
+        </div>
      );
 }
  
